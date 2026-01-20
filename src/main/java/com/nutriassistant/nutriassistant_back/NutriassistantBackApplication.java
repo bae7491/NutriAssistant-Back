20385245d8d510eecd1bf53172ad8a267d7ab2a1
bae7491 <bae7491@naver.com>
2026-01-20 15:58:40 +0900

    Spring boot 초기 설정 & DB 연결 구동 확인

---
+++ src/main/java/com/nutriassistant/nutriassistant_back/NutriassistantBackApplication.java
@@ -0,0 +1,13 @@
+package com.nutriassistant.nutriassistant_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriassistantBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriassistantBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutriassistant/nutriassistant_back/NutriassistantBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriassistant.nutriassistant_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriassistantBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
