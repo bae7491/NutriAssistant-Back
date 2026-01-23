1f8eb9f822037c3c4f35ba7ef80b5ac0f176cc08
bae7491 <bae7491@naver.com>
2026-01-23 14:54:49 +0900

    식단표 단가 계산 코드 추가

---
+++ src/main/java/com/nutriassistant/nutriassistant_back/repository/MenuCostRepository.java
@@ -0,0 +1,20 @@
+package com.nutriassistant.nutriassistant_back.repository;
+
+import com.nutriassistant.nutriassistant_back.entity.MenuCost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MenuCostRepository extends JpaRepository<MenuCost, Long> {
+
+    Optional<MenuCost> findByMenuName(String menuName);
+
+    List<MenuCost> findByCurrentYear(Integer currentYear);
+
+    @Query("SELECT m.menuName, m.price FROM MenuCost m")
+    List<Object[]> findAllMenuNameAndPrice();
+
+    boolean existsByMenuName(String menuName);
+}
