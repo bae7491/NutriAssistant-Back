bf4832159330af3a871428ab1c3f6f21072bfc95
bae7491 <bae7491@naver.com>
2026-01-23 15:55:54 +0900

    menu_id -> id로 이름 변경

--- src/main/java/com/nutriassistant/nutriassistant_back/DTO/MealMenuResponse.java
+++ src/main/java/com/nutriassistant/nutriassistant_back/DTO/MealMenuResponse.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record MealMenuResponse(
-        Long menuId,
+        Long id,
         LocalDate date,
         String type,
         String rice,
