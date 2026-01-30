6ec3280502c140037e5f7471fd9f8f2ebb3bd6e3
Jang Seok Jin <162329005+JSJ426@users.noreply.github.com>
2026-01-30 16:19:03 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
   cd /home/ec2-user/app
-  nohup java -jar build/libs/nutriassistant_back-0.0.1-SNAPSHOT.jar > /home/ec2-user/app.log 2>&1 &
+  nohup java -jar build/libs/*.jar > /home/ec2-user/app.log 2>&1 &
