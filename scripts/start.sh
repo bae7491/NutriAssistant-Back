75b6a932e53b3f4b25e4c332ff41c467e0b5f955
Jang Seok Jin <162329005+JSJ426@users.noreply.github.com>
2026-01-30 16:19:15 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-  cd /home/ec2-user/app
-  nohup java -jar build/libs/*.jar > /home/ec2-user/app.log 2>&1 &
+cd /home/ec2-user/app
+nohup java -jar build/libs/*.jar > /home/ec2-user/app.log 2>&1 &
