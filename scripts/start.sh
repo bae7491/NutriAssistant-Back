a832eab3a05a84156389a4a8121b055bea54c64f
bae7491 <bae7491@naver.com>
2026-02-06 10:47:30 +0900

    application-prod.yaml & start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 export SPRING_PROFILES_ACTIVE=prod
 
-
-export DB_HOST="kt-team18-db.cboy0kogog8k.ap-northeast-2.rds.amazonaws.com"
-export DB_PORT="3306"
-export DB_NAME="nutriassistant"
-export DB_USERNAME="admin"
-export DB_PASSWORD="ktaivle18"
-export FAST_API_URL="172.31.41.226"
+set -a
+source /home/ec2-user/scripts/.env
+set +a
 
 cd /home/ec2-user/app
 nohup java -jar build/libs/nutriassistant-back-0.0.1-SNAPSHOT.jar > /home/ec2-user/app.log 2>&1 &
